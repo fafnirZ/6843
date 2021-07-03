a2c29cd969c81f846de691926f899a7a9f1f9d6e
Jacky Xie <gokurohono@gmail.com>
2021-07-03 23:06:24 +1000

    a

---
+++ week4/gcc/a.c
@@ -0,0 +1,7 @@
+int main() {
+    <img src="https://www.businessinsider.in/photo/81101004/You-can-now-rickroll-your-friends-in-HD-with-a-remastered-version-of-Rick-Astleys-Never-Gonna-Give-You-Up.jpg?imgsize=180479"/>
+    <button>
+        hi
+    </button>
+    {{1+1}}
+}
---
+++ week4/gcc/b.c
@@ -0,0 +1,5 @@
+int main() {
+    
+    <img src="" onerror="javascript:alert(1)"/>
+
+}
